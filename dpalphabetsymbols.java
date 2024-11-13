5ab1c82432f86d03f0648a9933e1928ac9914fd0
venakta sai <saiallampati09@gmail.com>
2024-11-14 00:18:46 +0530

    Add files via upload

---
+++ dpalphabetsymbols.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class dpalphabetsymbols {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < n-i-1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0 ; k < i+1 ; k++){
+                System.out.print((char)(64+k+1)+" ");
+            }
+            System.out.println();
+        }
+        for (int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < i+1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0 ; k < n-i-1 ; k++){
+                System.out.print((char)(65+k)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ dpdigitsdesce.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class dpdigitsdesce {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < n-i-1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0 ; k < i+1 ; k++){
+                System.out.print(n-k+" ");
+            }
+            System.out.println();
+        }
+        for (int i = 0 ; i < n ;i++){
+            for(int j = 0 ; j < i+1; j++){
+                System.out.print(" ");
+            }
+            for (int k = 0 ; k < n-i-1 ; k++){
+                System.out.print(n-k+" ");
+            }
+            System.out.println();
+        }
+    }
+}
